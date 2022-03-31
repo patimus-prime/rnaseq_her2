dbfb871bdf2a058fd20164e48b247cd722597f08
patimus-prime <patrick.finnerty42@gmail.com>
2022-03-30 20:29:29 -0700

    mds ggplot graph enabled

--- initial.R
+++ initial.R
@@ -12,7 +12,8 @@ library(stringr)
 library(knitr)
 library(clipr) 
 library(edgeR)  # NOTE: Must capitalize R lolololol
-
+library(tibble)
+library(magrittr)
 
 setwd("~/rnaseq")
 rna_data <- read.csv("tcga.brca.rsem.csv",
@@ -82,6 +83,7 @@ rna_data_t_rd <- round(rna_data_t, digits = 0)
       # will require definition of design matrix
 
 #     B. Grab the positive and negative groups.
+      # Also this turns out to be useless, but maybe can use later
       
       positive_patients <- patient_IHC %>%
         filter(grepl('Positive', IHC.HER2)) 
@@ -90,101 +92,109 @@ rna_data_t_rd <- round(rna_data_t, digits = 0)
         filter(grepl('Negative', IHC.HER2))
       #567 patients
 
-# now we have our groups defined, can specify for edgeR below
-
-# 6. Into edgeR for normalization
+#     C. Not sure we got the groups correct, need to specify and 
+      # match up everything to reference the way edgeR does it.
+      # BEWARE: THIS IS CONFUSING BUT WORKS. LOOK INTO ALTERNATIVE LATER.
+      # PROPOSAL: USE DISTINCT() UPSTREAM, THEN PERHAPS WE CAN AVOID THE BACK AND FORTH
+      #           ... MERGE IS LIKELY NECESSARY THOUGH.
       
-      
+      # get only the patients that have IHC results
       y <- drop_na(patient_IHC)
-      
       matching_ids <- intersect(y$Patient.ID,colnames(rna_data_t_rd,))
       # therefore mathcing records contains all paitent ids with both IHC results and RNASeqs of primary tumors
-      
-      #matching_ids <- as.data.frame(matching_ids) 
-      
-       
-      
-      # so let's dump what's not in here for our below analysis
-      
-      xx <- select(rna_data_t_rd, matching_ids)
-      
-      # now a discrepancy between rna_data_t_rd and y, the no NA patient IHC data)
-      # so like... I guess 
-      
-      #u <- as.data.frame(matching_ids)
     
-    Patient.ID <- intersect(y$Patient.ID, matching_ids) # NEW MATCHING IDS
-    newMATCHES <- as.data.frame(Patient.ID)
-    #final <- as.data.frame(uu)
+      # get only the rna data that is from patients with IHC results
+      matched_rna_data <- select(rna_data_t_rd, matching_ids)
+      
+      # but awkwardly, we now have to use the one IDs that are shared
+      # to go back to our IDs and IHC results to see what matches to RNA.
+      # This seems excessively backwards and I'll take another look if time permits
+    
+      Patient.ID <- intersect(y$Patient.ID, matching_ids) # NEW MATCHING IDS
+      newMATCHES <- as.data.frame(Patient.ID)
     
    # only keeps the values in newMATCHES, left df, if extras detected
-    EXALTATION <- y %>%
+    matched_patient_IHC <- y %>%
       semi_join(newMATCHES, by = "Patient.ID")
     
     weird_rows <- y %>%
       anti_join(newMATCHES, by = "Patient.ID") # FIXME: need to investigate this later
     
-    EXCELLENCE <- EXALTATION %>%
-      anti_join(weird_rows, by = "Patient.ID")
+    # Remove duplicates -- look into the weird_rows above if time permits
+    matched_patient_IHC <- distinct(matched_patient_IHC) #weird lol
     
+    # EXALT!! so we now have all columns that match, after much derision and confusion
+
+# 6. Into edgeR for normalization
     
-    # YES!!!!!!!
-    eee <- distinct(EXALTATION) #weird lol
+    dim(matched_rna_data) # 20502 genes
     
-    # ok so we now have all columns that match, after much derision and confusion
-      
-      
-      # now let's get the ihc that matches up
-      d <- DGEList(counts = xx, group = factor(eee$IHC.HER2))
+    # A. First filter out genes expressed very little
+    
+    cutoff = 1 # counts per million; other cutoffs are used but this is what we'll start with
+    
+    keptGenes <- rowSums(cpm(matched_rna_data) > cutoff) >= ncol(matched_rna_data) #LAST PART == # LIBRARIES, SO 
+    #THE ENTIRE ROW HAS AT LEAST 1 READ/CPM PER LIBRARY
+    
+    matched_rna_data <- matched_rna_data[keptGenes,]
+    
+    dim(matched_rna_data) # 9105 genes with not very little expression after filtering
+                          # in case of confusion: we are removing columns (genes, counts), not patients
+    
+    # B. Create DGE List used in all of edgeR functions 
+    
+      # define the edgeR list object that matches everything up and makes life easy
+      d <- DGEList(counts = matched_rna_data, group = factor(matched_patient_IHC$IHC.HER2))
       d
       dim(d) #cool, everything is working
       
-      
+    # C. Normalization!!!!! CRITICAL STEP! ###########
+      # perform normalization, get factors based on edgeR's normalization algorithm (excluding VERY lowly expressed genes)
+      # btw edgeR uses TMM and a bunch of funky fresh math to do this
       d <- calcNormFactors(d)
       
-      d$samples
+      d$samples # more checking everything's A-OK
       
       ################
-      # BEHOLD! THE REMAINDER IS SIMPLE NOW THIS OBJECT IS DEFINED. WE MAY NOW GET CLUSTERS ETC.
+      # BEHOLD! THE REMAINDER IS SIMPLE NOW THIS OBJECT IS DEFINED, NORMALIZED. WE MAY NOW GET CLUSTERS ETC.
       # THEN WE CAN TAKE THE OUTPUT NORMALIZED FACTORS, THE MOST CRITICAL COMPONENTS AS DETERMINED 
       # BY PCA, AND THEN FEED THAT INTO THE LOGISTIC REGRESSION MODEL ALONG WITH THE ALLELE/COUNT THING
       #############
       
+   # D. Get normalized counts, can filter for most critical genes later and input to logistic regression model
+      norm_counts <- cpm(d) # Data look different,but those were raw counts and these are cpm from TMM
       
-      # cool
-      # BOOM THAT'S FINALLY DEFINED WHAT WHAT!!!!!
-      
+      # from: https://www.biostars.org/p/317701/
       #dge <- calcNormFactors(dge, method = "TMM")
       #tmm <- cpm(dge)
       
-      norm_counts <- cpm(d) # interesting how much stuff changed lol, but those were raw counts and these are cpm from TMM
       
+# 7. Further processes in edgeR ----------------
+      # without logs, very poor graph
+      #plotMDS(matched_rna_data)
       
+      # with logs, use only 500 top genes, use group labels instead of patient IDS
+      #plotMDS.DGEList(matched_rna_data, top = 500, labels = d$samples$group, gene.selection = "common", pch = ".") #,  pch = points(d$samples$group))
       
-      # INCREDIBLE -----------------------------
+      mds <- plotMDS.DGEList(matched_rna_data, top = 500, 
+                           method = "logFC"  , labels = d$samples$group, 
+                           gene.selection = "common", var.explained = TRUE)
+    
       
-      # these are the rnaseq results that match up with the shit
+      uberplot <- data.frame(Dim1 = mds$x, Dim2 = mds$y, Group = factor(d$samples$group))
+      ggplot(uberplot, aes(Dim1, Dim2, colour = Group)) + geom_point()
       
-      # filter the rows of genes that have very low expression 
-      # use cutoff of 1 counts per million; i'll try a few things but 
-      # a quick google wasn't enough to find a good rule of thumb, so going off edgeR guide
-      cutoff = 1 # counts per million
-      keptGenes <- rowSums(cpm(rna_data_t_rd) > cutoff) >= ncol(rna_data_t_rd) #LAST PART = # LIBRARIES, SO 
-                                                                            # THE ENTIRE ROW HAS AT LEAST 1 READ/CPM PER LIBRARY
-      
-      # now i think I might be skipping some steps... let's eat first 
-      # ok, let's get this stuff into a list and make sure we're starting from the beginning
-      super <- rna_data_t_rd[keptGenes,]
-      super <- calcNormFactors(super, method = "upperquartile")
-      
-     
-# Normalize ---------
-# I'll adjust for read depth (total row counts), but regular normalization doesn't seem possible;
-# I guess gene lengths could be imported to account for that, but this may have already been done during
-# generation of the data. Find out how this was generated!!
-
-#d <- t(apply(rna_data_only_genes, 1, function(x) x/sum(x)*1e6)) # this seems to work to divide by the sum of ea. row
-
-# however, is this valuable??...
-#
-
+     #  mds <- plotMDS(x)
+     #  toplot <- data.frame(Dim1 = mds$x, Dim2 = mds$y, Group = factor(paste0("Grp", rep(1:2, each = 3))))
+     #  library(ggplot2)
+     #  ggplot(toplot, aes(Dim1, Dim2, colour = Group)) + geom_point()
+     #  
+     #  toplot <- as.data.frame(mds@.Data[[3]]) %>%
+     #    left_join(d$samples$group)
+     # #mds@.Data[[3]]
+     #  mds@.Data[[3]] %>%
+     #    as.data.frame() %>%
+     #    #set_colnames(c("Dim1", "Dim2")) %>%
+     #    #rownames_to_column("SampleID") %>%
+     #    left_join(d$samples$group)
+     #  
